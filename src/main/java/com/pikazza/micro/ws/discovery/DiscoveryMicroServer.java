1725e2aa440bd0d8ba2c67c12060a4f89284fc19
Pikazza <karanit91@gmail.com>
2018-03-06 19:15:27 +0530

    updated changes

--- src/main/java/com/pikazza/micro/ws/discovery/DiscoveryMicroServer.java
+++ src/main/java/com/pikazza/micro/ws/discovery/DiscoveryMicroServer.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryMicroApplication {
+public class DiscoveryMicroServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryMicroApplication.class, args);
+		SpringApplication.run(DiscoveryMicroServer.class, args);
 	}
 }
